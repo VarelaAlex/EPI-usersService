8d68abc5969d264299020a01cacfa5a9e88c0045
uo271288 <uo271288@uniovi.es>
2024-05-12 19:41:07 +0200

    Finished register

--- routers/routerTeacher.js
+++ routers/routerTeacher.js
@@ -5,81 +5,47 @@ const jwt = require("jsonwebtoken");
 
 const routerTeachers = express.Router();
 
-let checkTeacherExist = async (email) => {
+routerTeachers.post("/", async (req, res) => {
+
+    let { name, lastName, email, password } = req.body;
+
+    if (!name?.trim()) {
+        return res.status(400).json({ error: { name: "signup.error.name" } });
+    }
+
+    if (!lastName?.trim()) {
+        return res.status(400).json({ error: { lastName: "signup.error.lastName" } });
+    }
+
+    if (!email?.trim()) {
+        return res.status(400).json({ error: { email: "signup.error.email" } });
+    }
+
+    if (!password?.trim()) {
+        return res.status(400).json({ error: { password: "signup.error.password.empty" } });
+    }
+
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        return res.status(400).json({ error: { email: "signup.error.email.format" } });
+    }
 
     database.connect();
 
+    let teacher = null;
     try {
         let teacherEmail = await database.query('SELECT email FROM teachers WHERE email = ?', [email]);
 
-        if (teacherEmail.length <= 0) {
-            return { error: { email: "login.error.email.notExist" } };
+        if (teacherEmail.length > 0) {
+            return res.status(404).json({ error: { email: "signup.error.email.repeated" } });
         }
-
+        teacher = await database.query('INSERT INTO teachers (name,lastName,email,password) VALUES (?,?,?,?)', [name, lastName, email, password]);
     } catch (e) {
-        return { error: { type: "internalServerError", message: e } };
-    } finally {
-        database.disconnect();
-    }
-};
-
-let checkTeacherPassword = async (email, password) => {
-
-    database.connect();
-
-    try {
-        let teacher = await database.query('SELECT id, email FROM teachers WHERE email = ? AND password = ?', [email, password]);
-
-        if (teacher.length === 0) {
-            return { error: { password: "login.error.password.incorrect" } };
-        }
-
-        return teacher;
-    } catch (e) {
-        return { error: { type: "internalServerError", message: e } };
-    } finally {
-        database.disconnect();
-    }
-};
-
-routerTeachers.post("/", async (req, res) => {
-
-    let { name, email, password } = req.body;
-    let errors = [];
-
-    if (email === undefined || email.trim().length === 0) {
-        errors.push("Email is required");
-    }
-    if (password === undefined || password.trim().length === 0) {
-        errors.push("Password is required");
-    }
-    if (name === undefined || name.trim().length === 0) {
-        errors.push("Name is required");
-    }
-    if (password?.length < 8) {
-        errors.push("Password must have at least 8 characters");
-    }
-    if (errors.length > 0) {
-        return res.status(400).json({ error: errors });
-    }
-
-    database.connect();
-
-    let insertedTeacher = null;
-    try {
-        teacherWithSameEmail = await database.query('SELECT email FROM teachers WHERE email = ?', [email]);
-
-        if (teacherWithSameEmail.length > 0) {
-            return res.status(400).json({ error: "There is already a teacher with this email" });
-        }
-        insertedTeacher = await database.query('INSERT INTO teacher (email,password,name) VALUES (?,?,?)', [email, password, name]);
-    } catch (e) {
-        return res.status(400).json({ error: "Internal server error" });
+        return res.status(500).json({ error: { type: "internalServerError", message: e } });
     } finally {
         database.disconnect();
     }
 
-    res.status(200).json({ inserted: insertedTeacher });
+    res.status(200).json({ inserted: teacher });
 });
 
 routerTeachers.post("/login", async (req, res) => {
@@ -102,7 +68,7 @@ routerTeachers.post("/login", async (req, res) => {
         let teacherEmail = await database.query('SELECT email FROM teachers WHERE email = ?', [email]);
 
         if (teacherEmail.length <= 0) {
-            return res.status(400).json({ error: { email: "login.error.email.notExist" } });
+            return res.status(404).json({ error: { email: "login.error.email.notExist" } });
         }
 
         teacher = await database.query('SELECT id, email FROM teachers WHERE email = ? AND password = ?', [email, password]);
@@ -111,7 +77,7 @@ routerTeachers.post("/login", async (req, res) => {
             return res.status(400).json({ error: { password: "login.error.password.incorrect" } });
         }
     } catch (e) {
-        return { error: { type: "internalServerError", message: e } };
+        return res.status(500).json({ error: { type: "internalServerError", message: e } });
     } finally {
         database.disconnect();
     }
@@ -124,8 +90,7 @@ routerTeachers.post("/login", async (req, res) => {
         "HYTEXJWTSecret");
     activeApiKeys.push(apiKey);
 
-
-    res.json({
+    res.status(200).json({
         apiKey: apiKey,
         id: teacher[0].id,
         email: teacher[0].email
@@ -139,7 +104,7 @@ routerTeachers.get("/disconnect", async (req, res) => {
         activeApiKeys.splice(apiKeyIndex, 1);
         res.status(200).json({ removed: true });
     } else {
-        return res.status(400).json({ error: "Teacher not found" });
+        return res.status(404).json({ error: "Teacher not found" });
     }
 });
 
