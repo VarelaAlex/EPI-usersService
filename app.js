de6497d7e748f7d72c58017df322181e9c01fe2b
uo271288 <uo271288@uniovi.es>
2024-09-28 01:37:52 +0200

    deploy

--- app.js
+++ app.js
@@ -18,15 +18,7 @@ const options = {
 const port = process.env.PORT;
 const app = express();
 
-const allowedOrigins = [
-    process.env.FRONTEND_URL
-];
-
-app.use(cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 
