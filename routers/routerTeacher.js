5ca1535bc35ff9cc04b8e72bc53ae03fba4f23fa
uo271288 <uo271288@uniovi.es>
2025-01-12 20:30:23 +0100

    Added more fields to teacher table

--- routers/routerTeacher.js
+++ routers/routerTeacher.js
@@ -13,69 +13,87 @@ const routerTeachers = express.Router();
 
 routerTeachers.post("/", async (req, res) => {
 
-    let { name, lastName, email, password } = req.body;
+    let {
+        name,
+        lastName,
+        email,
+        password,
+        teachingStage,
+        schoolType,
+        schoolLocation,
+        gender,
+        experienceYears,
+        community
+    } = req.body;
 
     if (!name?.trim()) {
-        return res.status(400).json({ error: { name: "signup.error.name" } });
+        return res.status(400).json({error: {name: "signup.error.name"}});
     }
 
     if (!lastName?.trim()) {
-        return res.status(400).json({ error: { lastName: "signup.error.lastName" } });
+        return res.status(400).json({error: {lastName: "signup.error.lastName"}});
     }
 
     if (!email?.trim()) {
-        return res.status(400).json({ error: { email: "signup.error.email" } });
+        return res.status(400).json({error: {email: "signup.error.email"}});
     }
 
     if (!password?.trim()) {
-        return res.status(400).json({ error: { password: "signup.error.password.empty" } });
+        return res.status(400).json({error: {password: "signup.error.password.empty"}});
     }
 
     if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        return res.status(400).json({ error: { email: "signup.error.email.format" } });
+        return res.status(400).json({error: {email: "signup.error.email.format"}});
     }
-
-    
 
     let teacher = null;
     try {
         let teacherEmail = await database.query('SELECT email FROM teachers WHERE email = ?', [email]);
 
         if (teacherEmail.length > 0) {
-            return res.status(404).json({ error: { email: "signup.error.email.repeated" } });
+            return res.status(404).json({error: {email: "signup.error.email.repeated"}});
         }
         const salt = await bcrypt.genSalt(12);
         const hashedPassword = await bcrypt.hash(password, salt);
-        teacher = await database.query('INSERT INTO teachers (name,lastName,email,password) VALUES (?,?,?,?)', [name, lastName, email, hashedPassword]);
+        teacher = await database.query('INSERT INTO teachers (name,lastName,email,password,teachingStage,schoolType,schoolLocation,gender,experienceYears,community) VALUES (?,?,?,?,?,?,?,?,?,?)',
+            [name,
+                lastName,
+                email,
+                hashedPassword,
+                teachingStage,
+                schoolType,
+                schoolLocation,
+                gender,
+                experienceYears,
+                community]);
     } catch (e) {
-        return res.status(500).json({ error: { type: "internalServerError", message: e } });
+        return res.status(500).json({error: {type: "internalServerError", message: e}});
     } finally {
-        
+
     }
 
-    res.status(200).json({ inserted: teacher });
+    res.status(200).json({inserted: teacher});
 });
 
 routerTeachers.post("/login", async (req, res) => {
 
-    let { email, password } = req.body;
+    let {email, password} = req.body;
 
     if (!email?.trim()) {
-        return res.status(400).json({ error: { email: "login.error.email.empty" } });
+        return res.status(400).json({error: {email: "login.error.email.empty"}});
     }
 
     if (!password?.trim()) {
-        return res.status(400).json({ error: { password: "login.error.password.empty" } });
+        return res.status(400).json({error: {password: "login.error.password.empty"}});
     }
 
-    
 
     let teacher = null;
     try {
         let teacherEmail = await database.query('SELECT email FROM teachers WHERE email = ?', [email]);
 
         if (teacherEmail.length <= 0) {
-            return res.status(404).json({ error: { email: "login.error.email.notExist" } });
+            return res.status(404).json({error: {email: "login.error.email.notExist"}});
         }
 
         teacher = await database.query('SELECT id, email, name, password FROM teachers WHERE email = ?', [email]);
@@ -83,12 +101,12 @@ routerTeachers.post("/login", async (req, res) => {
         const isMatch = await bcrypt.compare(password, teacher[0].password);
 
         if (!isMatch) {
-            return res.status(401).json({ error: { password: "login.error.password.incorrect" } });
+            return res.status(401).json({error: {password: "login.error.password.incorrect"}});
         }
     } catch (e) {
-        return res.status(500).json({ error: { type: "internalServerError", message: e } });
+        return res.status(500).json({error: {type: "internalServerError", message: e}});
     } finally {
-        
+
     }
 
     let user = {
@@ -98,13 +116,13 @@ routerTeachers.post("/login", async (req, res) => {
     };
 
     let tokens = generateTokens(user);
-    
+
     try {
         await database.query('INSERT INTO refreshTokens (refreshToken) VALUES (?)', [tokens.refreshToken]);
     } catch (e) {
-        return res.status(500).json({ error: { type: "internalServerError", message: e } });
+        return res.status(500).json({error: {type: "internalServerError", message: e}});
     } finally {
-        
+
     }
 
     res.status(200).json({
@@ -120,11 +138,11 @@ routerTeachers.get("/profile", authenticateToken, isTeacher, async (req, res) =>
 
     let teacher = null;
     try {
-        teacher = await database.query('SELECT name, lastName, email FROM teachers WHERE id = ?', [teacherId]);
+        teacher = await database.query('SELECT name,lastName,email,teachingStage,schoolType,schoolLocation,gender,experienceYears,community FROM teachers WHERE id = ?', [teacherId]);
     } catch (e) {
-        return res.status(500).json({ error: { type: "internalServerError", message: e } });
+        return res.status(500).json({error: {type: "internalServerError", message: e}});
     } finally {
-        
+
     }
 
     res.status(200).json(teacher[0]);
@@ -133,27 +151,33 @@ routerTeachers.get("/profile", authenticateToken, isTeacher, async (req, res) =>
 routerTeachers.put("/profile", authenticateToken, isTeacher, async (req, res) => {
 
 
-    let { name, lastName, email/*, password*/ } = req.body;
+    let {
+        name,
+        lastName,
+        email,
+        teachingStage,
+        schoolType,
+        schoolLocation,
+        gender,
+        experienceYears,
+        community
+    } = req.body;
     let teacherId = req.user.id;
 
     if (!name?.trim()) {
-        return res.status(400).json({ error: { name: "profile.error.name" } });
+        return res.status(400).json({error: {name: "profile.error.name"}});
     }
 
     if (!lastName?.trim()) {
-        return res.status(400).json({ error: { lastName: "profile.error.lastName" } });
+        return res.status(400).json({error: {lastName: "profile.error.lastName"}});
     }
 
     if (!email?.trim()) {
-        return res.status(400).json({ error: { email: "profile.error.email.mandatory" } });
+        return res.status(400).json({error: {email: "profile.error.email.mandatory"}});
     }
 
-    /*if (password && !password?.trim()) {
-        return res.status(400).json({ error: { name: "profile.error.password" } });
-    }*/
-
     if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        return res.status(400).json({ error: { email: "profile.error.email.format" } });
+        return res.status(400).json({error: {email: "profile.error.email.format"}});
     }
 
     let teacher = null;
@@ -163,39 +187,34 @@ routerTeachers.put("/profile", authenticateToken, isTeacher, async (req, res) =>
             [email, teacherId]
         );
 
-        if ( teacherEmail.length > 0 ) {
-            return res.status(404).json({ error: { email: "profile.error.email.repeated" } });
+        if (teacherEmail.length > 0) {
+            return res.status(404).json({error: {email: "profile.error.email.repeated"}});
         }
 
-        //const salt = await bcrypt.genSalt(12);
-        //const hashedPassword = await bcrypt.hash(password, salt);
-
-        /* 'UPDATE teachers \
-            SET \
-                name = IFNULL(?, name), \
-                lastName = IFNULL(?, lastName), \
-                email = IFNULL(?, email), \
-                password = IFNULL(?, password) \
-            WHERE id = ?'
-             */
         teacher = await database.query(
             'UPDATE teachers \
             SET \
                 name = IFNULL(?, name), \
                 lastName = IFNULL(?, lastName), \
-                email = IFNULL(?, email) \
-            WHERE id = ?', [name, lastName, email, teacherId]);
+                email = IFNULL(?, email), \
+                teachingStage = IFNULL(?, teachingStage), \
+                schoolType = IFNULL(?, schoolType), \
+                schoolLocation = IFNULL(?, schoolLocation), \
+                gender = IFNULL(?, gender), \
+                experienceYears = IFNULL(?, experienceYears), \
+                community = IFNULL(?, community) \
+            WHERE id = ?', [name, lastName, email, teachingStage, schoolType, schoolLocation, gender, experienceYears, community, teacherId]);
     } catch (e) {
-        return res.status(500).json({ error: { type: "internalServerError", message: e } });
+        return res.status(500).json({error: {type: "internalServerError", message: e}});
     } finally {
-        
+
     }
 
-    res.status(200).json({ updated: teacher });
+    res.status(200).json({updated: teacher});
 });
 
 routerTeachers.get("/checkLogin", authenticateToken, isTeacher, async (req, res) => {
-    return res.status(200).json({ user: req.user });
+    return res.status(200).json({user: req.user});
 });
 
 module.exports = routerTeachers;
